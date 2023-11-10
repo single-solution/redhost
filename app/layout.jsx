a0d3f799efe77c4c12fefce1ad228af559b70ddb
Muhammad Faiq Nadeem <chfaiqnadeem@gmail.com>
2023-11-10 15:10:22 +0500

    feat: code refactoring

--- app/layout.jsx
+++ app/layout.jsx
@@ -26,23 +26,23 @@ export default function RootLayout({ children }) {
 				<Footer />
 			</body>
 
-			<Script defer src="/assets/js/vendors/jquery.min.js" />
-			<Script defer src="/assets/js/app.js" />
-			<Script defer src="/assets/js/vendors/popper.min.js" />
-			<Script defer src="/assets/js/vendors/bootstrap.min.js" />
-			<Script defer src="/assets/js/vendors/easing.min.js" />
-			<Script defer src="/assets/js/vendors/swiper.min.js" />
-			<Script defer src="/assets/js/vendors/masonry.min.js" />
-			<Script defer src="/assets/js/vendors/bootstrap-slider.min.js" />
-			<Script defer src="/assets/js/vendors/magnific-popup.min.js" />
-			<Script defer src="/assets/js/vendors/waypoints.min.js" />
-			<Script defer src="/assets/js/vendors/jquery.counterup.min.js" />
-			<Script defer src="/assets/js/vendors/isotope.pkgd.min.js" />
-			<Script defer src="/assets/js/vendors/countdown.min.js" />
-			<Script defer src="/assets/js/vendors/jquery.dataTables.min.js" />
-			<Script defer src="/assets/js/vendors/dataTables.bootstrap5.min.js" />
-			<Script defer src="/assets/js/vendors/dataTables.responsive.min.js" />
-			<Script defer src="/assets/js/vendors/dataTables.buttons.min.js" />
+			<Script defer src="assets/js/vendors/jquery.min.js" />
+			<Script defer src="assets/js/app.js" />
+			<Script defer src="assets/js/vendors/popper.min.js" />
+			<Script defer src="assets/js/vendors/bootstrap.min.js" />
+			<Script defer src="assets/js/vendors/easing.min.js" />
+			<Script defer src="assets/js/vendors/swiper.min.js" />
+			<Script defer src="assets/js/vendors/masonry.min.js" />
+			<Script defer src="assets/js/vendors/bootstrap-slider.js" />
+			<Script defer src="assets/js/vendors/magnific-popup.js" />
+			<Script defer src="assets/js/vendors/waypoints.js" />
+			<Script defer src="assets/js/vendors/counterup.js" />
+			<Script defer src="assets/js/vendors/isotop.pkgd.min.js" />
+			<Script defer src="assets/js/vendors/countdown.min.js" />
+			<Script defer src="assets/js/vendors/jquery.dataTables.min.js" />
+			<Script defer src="assets/js/vendors/dataTables.bootstrap5.min.js" />
+			<Script defer src="assets/js/vendors/dataTables.responsive.min.js" />
+			<Script defer src="assets/js/vendors/dataTables.buttons.min.js" />
 			<Script defer src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" />
 		</html>
 	);
